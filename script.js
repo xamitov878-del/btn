3308bb0ec8741e9e056700cf7ab032fbfcc5689f
xamitov878-del <xamitov878@gmail.com>
2026-04-21 16:57:13 +0500

    tugilandoi

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let elBox = document.querySelector(".span")
 let elBtn = document.querySelector(".check")
-elBtn.addEventListener("click", ()=>{
+elBox.addEventListener("click", ()=>{
     elBox.classList.toggle("span2")
     elBtn.classList.toggle("check2")
     document.body.classList.toggle("dark")
